934502c59cffd749251e216b83ad40c63c1a32f9
areyes2853 <46231827+areyes2853@users.noreply.github.com>
2025-04-07 20:54:54 -0400

    datachange

--- src/components/weatherData/WeatherData.jsx
+++ src/components/weatherData/WeatherData.jsx
@@ -5,8 +5,8 @@ import React from 'react';
 const WeatherData = ({ conditions, time }) => {
   return (
     <>
-      <p><span>conditions: </span>{conditions}</p>
-      <p><span>time: </span>{time}</p>
+      <p><span>Conditions: </span>{conditions}</p>
+      <p><span>Time: </span>{time}</p>
     </>
   );
 };
